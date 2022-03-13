a03f50b666f6102dd2fb6ce8c90bf280105cf50b
Asus <Asus@DESKTOP-AMRUMOT>
2022-03-13 21:19:45 +0300

    add function and extra (must fix filtr and reduce)

--- Extra tasks/2.2/index.js
+++ Extra tasks/2.2/index.js
@@ -1,4 +1,4 @@
-
+/* 
 var sp = document.querySelectorAll('.msq,.jfk');
 sp.forEach(d=>{
 d.lastElementChild.onmouseover=function(event){this.style.color="#ff6f32";d.firstElementChild.style.fill="#ff6f32"};
@@ -6,5 +6,5 @@ d.firstElementChild.onmouseover=function(event){this.style.fill="#ff6f32";d.last
 
 d.lastElementChild.onmouseout=function(event){this.style.color="#0c73fe";d.firstElementChild.style.fill="#9ea9b7"};
 d.firstElementChild.onmouseout=function(event){this.style.fill="#9ea9b7";d.lastElementChild.style.color="#0c73fe"};
-});
+}); */
 
---
+++ UI/index.js
@@ -0,0 +1,73 @@
+
+import {getTweets,getTweet,validateTweet,editTweet,user} from './modules/main_module.js'
+
+ 
+window.onload = function () {
+    let current_user=document.getElementsByClassName('name')[0];
+    current_user.innerHTML=user;
+    let list = document.getElementsByClassName('twit-list')[0];
+   /* getTweets().forEach(twit => {
+        var div = document.createElement('div');
+        div.className = "twit-item big-shadow border ";
+        div.innerHTML = '<div class="twit-header"><p class="author-name bold-text">' + twit.author + '</p><p class="date grey-text text-small">' + getDate(twit.createdAt) + '</p></div><p class="twit-text">' + addHashtags(twit.text) + '</p><div class="twit-footer"><p class="comm grey-text text-small">Комментарии: ' + commentsCount(twit.comments) + '</p><div class="edit  grey-text text-small"><p> Ред.</p><object type="image/svg+xml" data="img/edit.svg"></object></div></div><div class="del"><object type="image/svg+xml" data="img/close_twit.svg" ></object></div>';
+        twit.author == user ? div.className += "view" : false;
+        div.onclick = clickTwit;
+        list.appendChild(div)
+    })*/
+
+};
+function getDate(date) {
+    var month = [
+        'Января',
+        'Февраля',
+        'Марта',
+        'Апреля',
+        'Мая',
+        'Июня',
+        'Июля',
+        'Августа',
+        'Сентября',
+        'Ноября',
+        'Декабря',
+    ];
+    
+    return date.getDate() + ' ' + month[date.getMonth() - 1] + ' ' + date.getFullYear() + ' ' + date.toLocaleTimeString().substring(0, 5);
+}
+function addHashtags(text) {
+    let span = document.createElement('span');
+    span.className = "hash";
+    let regexp = /([#])\w*[А-я]*/g;
+    return text.replace(regexp, replacer)
+}
+function replacer(match) {
+
+    return '<span class="hash">' + match + '</span>';
+}
+function commentsCount(arr) {
+    return arr.length;
+}
+function clickTwit(e) {
+    let target = e.target;
+
+    /* if (target.classList.contains('edit')|| target.tagName=='OBJECT') {
+        alert('edit')
+    }
+    if (target.classList.contains('del')) {
+        alert('del')
+    } */
+
+    alert(target)
+}
+//console.log(getTweets())
+//console.log(getTweets(10))
+//console.log(getTweets(10,5))
+
+//console.log(getTweet(5))
+//console.log(getTweet(50))
+//let tw =getTweet(5);
+//(tweets.map(twit => twit.id ==tw.id)).includes('true');
+//validateTweet(getTweet(3))
+
+//console.log(editTweet(1,"#hi"));
+//console.log(getTweet(1));
+
