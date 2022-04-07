d08bccedb4a2b3b4336e32eba7fb6a34f7c076c9
Asus <Asus@DESKTOP-AMRUMOT>
2022-04-07 11:53:57 +0300

    all work

--- UI/js/TweetsController.js
+++ UI/js/TweetsController.js
@@ -15,17 +15,27 @@ class TweetsController {
   constructor(tweetsDef, headerId, tweetFeedViewId, tweetViewId, filterViewId, logViewId) {
     this.tweetCollection = new TweetCollection(tweetsDef);
     this.headerView = new HeaderView(headerId);
-    this.headerView.setNewUser(this.setCurrentUser(this));///???
-    this.headerView.getUser = this.getCurrentUser(this);
+    //this.headerView.setNewUser = this.setCurrentUser(this);///???
+    this.headerView.username = this.getCurrentUser;
     this.tweetFeedView = new TweetFeedView(tweetFeedViewId);
+    this.tweetFeedView.getUsername = this.getCurrentUser;
     this.tweetView = new TweetView(tweetViewId, filterViewId);
+    this.tweetView.getUsername = this.getCurrentUser;
     this.filterView = new FilterView(filterViewId);
     this.filterView.authors = this.getAuthors();
     this.logView = new LogView(logViewId);
+    this.skip = 0;
+    this.top = 10;
+    this.filterParams = {};
+    this.tweetFeedView.skip = this.skip;
   }
 
   getCurrentUser = function () {
     return TweetCollection.user;
+  };
+
+  displayHeader = function () {
+    this.headerView.display();
   };
 
   setCurrentUser = function (userNew) {
@@ -33,8 +43,18 @@ class TweetsController {
     this.headerView.display();
   };
 
-  loginPageLoad = function (obj) {
-    this.logView.display(obj);
+  skipTweets = function () {
+    this.skip += 10;
+    this.getFeed(this.skip, this.top, this.filterParams);
+  };
+
+  loginPageLoad = function () {
+    this.logView.display();
+    this.logView.bindControllerTweets(this.setCurrentUser.bind(this), this.getFeed.bind(this));
+  };
+
+  regPageLoad = function () {
+
   };
 
   filterBlockLoad = function () {
@@ -42,8 +62,17 @@ class TweetsController {
   };
 
   getSearchTweets = function (filterConfig) {
-    console.log(filterConfig);
-    this.getFeed(0, 10, filterConfig);
+    this.filterParams = filterConfig;
+    this.getFeed(this.skip, this.top, this.filterParams);
+  };
+
+  bindFunction = function () {
+    this.filterView.bindControllerTweets(this.getSearchTweets.bind(this));
+    this.headerView.bindControllerTweets(
+      this.loginPageLoad.bind(this),
+      this.getFeed.bind(this),
+      this.setCurrentUser.bind(this),
+    );
   };
 
   getFeed = function (skip = 0, top = 10, filterConfig = {}) {
@@ -53,8 +82,8 @@ class TweetsController {
       this.editTweet.bind(this),
       this.showTweet.bind(this),
       this.addTweet.bind(this),
+      this.skipTweets.bind(this),
     );
-    this.filterView.bindControllerTweets(this.getSearchTweets.bind(this));
   };
 
   getAuthors = function () {
@@ -73,11 +102,15 @@ class TweetsController {
     if (this.tweetCollection.remove(id)) this.getFeed();
   };
 
+  addComment = function (id, text) {
+    if (this.tweetCollection.addComment(id, text)) this.showTweet(id);
+  };
+
   showTweet = function (id) {
     const tw = this.tweetCollection.get(id);
     if (tw) {
       this.tweetView.display(tw);
-      this.tweetView.bindControllerTweets(this.getFeed.bind(this));
+      this.tweetView.bindControllerTweets(this.getFeed.bind(this), this.addComment.bind(this));
     }
   };
 }
