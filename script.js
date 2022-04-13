0ff41231a038353f1c0106f7ded65fc2d723d469
Asus <Asus@DESKTOP-AMRUMOT>
2022-04-13 20:34:31 +0300

    add date to footer

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ import TweetFeedApiService from './js/TweetFeedApiService.js';
 const server = new TweetFeedApiService('https://jslabapi.datamola.com');
 const tweetController = new TweetsController('headerId', 'main', 'main', 'filter', 'main', 'main', server, 'main');
 
-
+fetch('https://api.github.com/repos/DaryaMarmysh/DataMola/branches/master').then((res) => {
+    if (res.ok) {
+     return res.json();
+    } return Promise.reject(res);
+  })
+  .then((data)=>document.querySelector('.ver').innerHTML=data.commit.commit.committer.date.slice(0,10))
+  .catch((error) => console.log(error.status));
 //server.setLogintUser();
 //server.addNewTweet('new tweet from vs code')
 //console.log(server.token);
